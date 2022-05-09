1cce888438c593a2d267583e11c262b03ecaaa07
zhaoz <1229700931@qq.com>
2022-05-09 21:24:25 +0800

    deps: 修改ACCESS_TOKEN

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -15,7 +15,7 @@ const msg = require('fs').readFileSync('.git/COMMIT_EDITMSG', 'utf-8').trim()
 // chore: 其他修改（不在上述类型中的修改）
 // release: 发版
 // deps: 依赖相关的修改
-// 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
+// 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
 const commitRE =
   /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps)(\(.+\))?: .{1,50}/
 const mergeRe = /^(Merge pull request|Merge branch)/
